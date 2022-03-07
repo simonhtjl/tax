b14bcaeb58affe33da76a1f7c3c1c3ef47d491f3
simonhtjl <simonhtjl@gmail.com>
2022-03-07 12:53:16 +0700

    uplaod v1

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
+const ptkp = 50000000
+let sumOfTax
+
 function calculateTax(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount) {
+    
+    if(annualIncome > 0 & annualIncome <= 200000000){
+        sumOfTax = 10/100*(annualIncome - ptkp)
+    }else if(annualIncome > 0 & annualIncome <= 200000000 ){
+        sumOfTax = 10/100*(annualIncome - ptkp)
+    }else if(annualIncome > 200000001 & annualIncome <= 450000000 ){
+        sumOfTax = 15/100*(annualIncome - ptkp)
+    }else if(annualIncome > 450000000 ){
+        sumOfTax = 20/100*(annualIncome - ptkp)
+    }
+
     console.log(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount)
-    return 500000
+    return sumOfTax
 }
