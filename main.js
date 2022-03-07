2f35674d79627dc8a0d68ef9901e080b1eec4848
simonhtjl <simonhtjl@gmail.com>
2022-03-07 13:01:48 +0700

    upload

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ let sumOfTax
 
 function calculateTax(taxYear, name, annualIncome, marriageStatus, dependentChildrenCount) {
     
-    if(annualIncome > 0 & annualIncome <= 200000000){
+    if(annualIncome <= 50000000){
+        sumOfTax = 0
+    }else if(annualIncome > 0 & annualIncome <= 200000000){
         sumOfTax = 10/100*(annualIncome - ptkp)
     }else if(annualIncome > 0 & annualIncome <= 200000000 ){
         sumOfTax = 10/100*(annualIncome - ptkp)
